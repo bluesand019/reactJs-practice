cf63b4ecd42709b70ee2342770b681bca243e2bf
Kowsor Dip <48145599+bluesand019@users.noreply.github.com>
2026-03-10 05:25:49 +0600

    Added files

---
+++ useReducer hook/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { useReducer } from 'react';
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case ACTIONS.INCREMENT:
+      return {count: state.count + 1}
+    case ACTIONS.DECREMENT:
+      return {count: state.count - 1}
+    default:
+      return state
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, {count:0});
+
+  const increment = () => {
+    dispatch({type: ACTIONS.INCREMENT});
+  }
+  const decrement = () => {
+    dispatch({type: ACTIONS.DECREMENT});
+  }
+
+  return (
+    <div className="App">
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+export default App;
